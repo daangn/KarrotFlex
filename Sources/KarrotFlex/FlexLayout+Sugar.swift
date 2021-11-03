298102529c04f5bf029e589164c07d82f2a66e66
Geektree0101 <h2s1880@gmail.com>
2021-11-03 21:23:17 +0900

    add flex center

--- Sources/KarrotFlex/FlexLayout+Sugar.swift
+++ Sources/KarrotFlex/FlexLayout+Sugar.swift
@@ -84,3 +84,83 @@ public func FlexInset(_ flex: Flex, all: CGFloat) -> Flex {
   return flex
     .padding(all)
 }
+
+// MARK: - Center
+
+public enum FlexCenterOption {
+  case X
+  case Y
+  case XY
+}
+
+@discardableResult
+public func FlexCenter(_ flex: Flex, option: FlexCenterOption, _ child: @escaping (Flex) -> Flex) -> Flex {
+  switch option {
+  case .X:
+    return flex
+      .addItem()
+      .width(100%)
+      .height(100%)
+      .direction(.row)
+      .justifyContent(.center)
+      .define {
+        // NOTE: discard result
+        _ = child($0)
+      }
+  case .Y:
+    return flex
+      .addItem()
+      .width(100%)
+      .height(100%)
+      .direction(.column)
+      .justifyContent(.center)
+      .define {
+        // NOTE: discard result
+        _ = child($0)
+      }
+  case .XY:
+    return flex
+      .addItem()
+      .width(100%)
+      .height(100%)
+      .justifyContent(.center)
+      .define {
+        child($0).alignSelf(.center)
+      }
+  }
+}
+
+@discardableResult
+public func FlexCenter(_ flex: Flex, option: FlexCenterOption, view: UIView) -> Flex {
+  switch option {
+  case .X:
+    return flex
+      .addItem()
+      .width(100%)
+      .height(100%)
+      .direction(.row)
+      .justifyContent(.center)
+      .define {
+        $0.addItem(view)
+      }
+  case .Y:
+    return flex
+      .addItem()
+      .width(100%)
+      .height(100%)
+      .direction(.column)
+      .justifyContent(.center)
+      .define {
+        $0.addItem(view)
+      }
+  case .XY:
+    return flex
+      .addItem()
+      .width(100%)
+      .height(100%)
+      .justifyContent(.center)
+      .define {
+        $0.addItem(view).alignSelf(.center)
+      }
+  }
+}
